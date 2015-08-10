54ce029c87a52d40cde2b288f75e957face99eec
chniccs <chniccs@163.com>
2015-08-10 22:37:42 +0800

    添加listview自定义菜单，且开放接口给使用者实现点击菜单事件

--- src/com/yufenit/slidemenu/MainActivity.java
+++ src/com/yufenit/slidemenu/MainActivity.java
@@ -1,15 +1,79 @@
 package com.yufenit.slidemenu;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.yufenit.slidemenu.ui.SlideMenuView;
 
 public class MainActivity extends Activity {
+
+	private SlideMenuView mSmv;
+	private List<Object> list;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		list = new ArrayList<Object>();
+
+		for (int i = 1; i < 5; i++) {
+			list.add("菜单" + i);
+		}
+		
+		//TODO 此处是通过静态方法先加载数据，在后台通过异步方法实现
+		SlideMenuView.setAdapter_listView(new MyAdapter(), list);
+		//加载好数据后再创建view并添加到此activity中
+		mSmv = new SlideMenuView(getApplicationContext());
+		addContentView(mSmv, new LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT));
+
+	}
+
+	public class MyAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			return list.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return list.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			TextView tv;
+			if (convertView == null) {
+
+				tv = new TextView(MainActivity.this);
+
+				convertView = tv;
+			} else {
+				tv = (TextView) convertView;
+			}
+
+			tv.setText(list.get(position).toString());
+
+			return tv;
+		}
+
 	}
 
 }
--- src/com/yufenit/slidemenu/ui/SlideMenuView.java
+++ src/com/yufenit/slidemenu/ui/SlideMenuView.java
@@ -1,12 +1,18 @@
 package com.yufenit.slidemenu.ui;
 
+import java.util.List;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.yufenit.slidemenu.R;
 
@@ -15,6 +21,10 @@ public class SlideMenuView extends ViewGroup {
 	private View menuContainer;
 	private View contentsContainer;
 	private int startX;
+	private static BaseAdapter adapter;
+	private static List<Object> list;
+	
+	private onSlideMenuViewInit listener;
 
 	public SlideMenuView(Context context) {
 		super(context);
@@ -38,9 +48,38 @@ public class SlideMenuView extends ViewGroup {
 		contentsContainer = View.inflate(context, R.layout.content, null);
 		menuLl = (LinearLayout) menuContainer.findViewById(R.id.ll);
 		contentsLl = (LinearLayout) contentsContainer.findViewById(R.id.content_ll);
+		
+		initListView(menuContainer);
 
 		addView(contentsContainer);
 		addView(menuContainer);
+	}
+	/**
+	 * 实现设置菜单列表的方法
+	 * @param menuContainer2
+	 */
+	private void initListView(View menuContainer2) {
+		
+		mLv = (ListView) menuContainer.findViewById(R.id.menu_lv);
+		
+		System.out.println("initListView执行了");
+		if(adapter!=null&& list!=null){
+			
+			mLv.setAdapter(adapter);
+			//设置listView的点击事件
+			mLv.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+					
+					listener.onItemClick(position);
+					
+				}
+				
+			});
+		}
+		
 	}
 
 	@Override
@@ -70,6 +109,7 @@ public class SlideMenuView extends ViewGroup {
 	int pos = 0;
 	private LinearLayout menuLl;
 	private LinearLayout contentsLl;
+	private ListView mLv;
 
 	@SuppressLint("NewApi")
 	@Override
@@ -110,7 +150,6 @@ public class SlideMenuView extends ViewGroup {
 			contentsLl.setScaleX(1.0f-i/6);
 			contentsLl.setScaleY(1.0f-i/6);
 
-			System.out.println(pos);
 			startX = (int) event.getRawX();
 
 			break;
@@ -141,6 +180,28 @@ public class SlideMenuView extends ViewGroup {
 		}
 
 		return true;
+	}
+	/**
+	 * 设置展示的数据及适配器的方法
+	 * @param adapter 适配器，不能为空，与list结合展示数据
+	 * @param list 集合存储数据，不能为空
+	 */
+	//TODO  设置回调
+	public static void setAdapter_listView(BaseAdapter madapter,List<Object> mlist){
+		System.out.println("setAdapter_listView执行了");
+		adapter=madapter;
+		list=mlist;
+	}
+	
+	/**
+	 *  回调方法的接口
+	 * @author Administrator
+	 *
+	 */
+	interface onSlideMenuViewInit{
+		
+		void onItemClick(int position);
+		
 	}
 
 }
